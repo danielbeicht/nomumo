ce715114c3166ca16d91a0a74c3f78f45bf94dcd
Daniel <danielbeicht@mz-host.de>
2016-08-18 17:16:12 +0200

    implemented cookies to save selected language

--- app/components/languageCtrl.js
+++ app/components/languageCtrl.js
@@ -5,14 +5,14 @@
         .module('nomumo')
         .controller('languageCtrl', languageCtrl);
 
-    languageCtrl.$inject = ['$scope', '$translate'];
-    function languageCtrl($scope, $translate) {
-
+    languageCtrl.$inject = ['$scope', '$translate', '$cookies'];
+    function languageCtrl($scope, $translate, $cookies) {
 
         $scope.$watch('language', function(newValue, oldValue) {
             if (newValue){
                 $translate.use(newValue).then(function (newValue) {
                     console.log("Sprache zu " + newValue + " gewechselt.");
+                    $cookies.put('language', newValue);
                 }, function (key) {
                     console.log("Irgendwas lief schief.");
                 });
@@ -20,7 +20,9 @@
             $scope.location = $translate.use();
         });
 
-
+        if ($cookies.get('language')){
+            $scope.language = $cookies.get('language');
+        }
 
     }
 })();
