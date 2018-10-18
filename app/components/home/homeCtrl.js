2a1a5bcc447dfc997bc1c1f98dfcdf786fb906c6
danielbeicht <daniel.beicht@outlook.com>
2018-10-18 18:42:05 +0200

    removed warnings in homeCtrl

--- app/components/home/homeCtrl.js
+++ app/components/home/homeCtrl.js
@@ -20,18 +20,18 @@
             } else {
                 return "eng";
             }
-        }
+        };
 
 
         $rootScope.$on('$stateChangeStart',
-            function(event, toState, toParams, fromState, fromParams){
-                if (toState.url == "/pollDate"){
+            function(event, toState){
+                if (toState.url === "/pollDate"){
                     if (!$scope.formData.title || !$scope.formData.name || !$scope.formData.email){
                         event.preventDefault();
                     }
                 }
-                else if (toState.url == "/pollDateTime"){
-                    if ($scope.formData.dates.length==0){
+                else if (toState.url === "/pollDateTime"){
+                    if ($scope.formData.dates.length === 0){
                         event.preventDefault();
                     }
                 }
@@ -43,11 +43,11 @@
             if ($scope.formData.dates.length > 0 && $scope.formData.title && $scope.formData.name && $scope.formData.email && !$scope.checkDuplicates()) {
 
 
-                for (var i = 0; i < $scope.formData.dates.length; i++) {
-                    var length = $scope.formData.dates[i].time.length;
-                    for (var j = length - 1; j >= 0; j--) {
+                for (let i = 0; i < $scope.formData.dates.length; i++) {
+                    let length = $scope.formData.dates[i].time.length;
+                    for (let j = length - 1; j >= 0; j--) {
                         console.log("test");
-                        if ($scope.formData.dates[i].time[j] == "") {
+                        if ($scope.formData.dates[i].time[j] === "") {
                             $scope.formData.dates[i].time.splice(j, 1);
                         } else {
                             console.log("WTF");
@@ -56,12 +56,12 @@
                     console.log($scope.formData.dates[i].time.length);
                 }
 
-                var parameter = JSON.stringify($scope.formData);
+                let parameter = JSON.stringify($scope.formData);
                 console.log(parameter);
 
-                $http.post("/createPoll", parameter).success(function (data, config) {
+                $http.post("/createPoll", parameter).success(function (data) {
                     $window.location.href = '/created/' + data.pollID;
-                }).error(function (data, status, headers, config) {
+                }).error(function () {
                     console.log("Error creating poll.")
                 });
             }
@@ -81,7 +81,7 @@
             } else {
                 console.log("False");
             }
-        }
+        };
 
 
         $scope.goToUhrzeit = function(){
@@ -90,37 +90,36 @@
                 $scope.warningMessage = false;
                 $state.go('home.pollDateTime');
             } else {
-                $scope.warningText = "Bitte wählen Sie mindestens ein Datum."
+                $scope.warningText = "Bitte wählen Sie mindestens ein Datum.";
                 $scope.warningMessage = true;
             }
+        };
 
-        }
-
-        $scope.getTimeModel = function(index){
+        $scope.getTimeModel = function(){
             return "ok";
-        }
+        };
 
 
         // Watcher
         $scope.$watch('formData.dt', function(newValue, oldValue) {
             console.log(newValue);
             if (newValue !== oldValue) {
-                var datefilter = $filter('date');
-                var formattedDate = datefilter($scope.formData.dt, 'yyyy/MM/dd');
+                let datefilter = $filter('date');
+                let formattedDate = datefilter($scope.formData.dt, 'yyyy/MM/dd');
                 console.log(formattedDate);
                 if (!isDateInDates(formattedDate)){
                     $scope.warningMessage = false;
-                    var obj = new Object();
+                    let obj = {};
                     obj.date = formattedDate;
                     obj.time = [];
-                    for (var i=0; i<$scope.timesCount; i++){
+                    for (let i=0; i<$scope.timesCount; i++){
                         obj.time.push("");
                     }
 
                     $scope.formData.dates.push(obj);
                     $scope.formData.dates.sort(compare);
                 } else {
-                    $scope.warningText = "Das Datum " + formattedDate + " wurde bereits gewählt."
+                    $scope.warningText = "Das Datum " + formattedDate + " wurde bereits gewählt.";
                     $scope.warningMessage = true;
                 }
             }
@@ -140,7 +139,7 @@
 
         function isDateInDates(date){
             for (var i=0; i<$scope.formData.dates.length; i++){
-                if ($scope.formData.dates[i].date == date){
+                if ($scope.formData.dates[i].date === date){
                     return true;
                 }
             }
@@ -149,30 +148,30 @@
 
         $scope.removeDate = function(date){
             $scope.formData.dates.splice($scope.formData.dates.indexOf(date), 1);
-        }
+        };
 
         $scope.addTime = function(){
             if ($scope.timesCount >= 4){
-                var offset = 200*($scope.timesCount-3);
-                var pixels = 1000+offset;
+                let offset = 200*($scope.timesCount-3);
+                let pixels = 1000+offset;
                 document.getElementById("limit").style.width = pixels + "px";
             }
             $scope.timesCount++;
-            var obj = new Object();
+            let obj = {};
                 obj.time = $scope.timesCount;
 
             obj.model = parseInt($scope.timesCount-1);
             $scope.times.push(obj);
 
-            for (var i=0; i<$scope.formData.dates.length; i++){
+            for (let i=0; i<$scope.formData.dates.length; i++){
                 $scope.formData.dates[i].time.push("");
             }
-        }
+        };
 
 
         $scope.checkDuplicates = function (){
-            var errorExists = false;
-            for (var i=0; i<$scope.formData.dates.length; i++){
+            let errorExists = false;
+            for (let i=0; i<$scope.formData.dates.length; i++){
                 errorExists = check($scope.formData.dates[i].time);
                 if (errorExists){
                     $scope.warningMessage=true;
@@ -189,15 +188,14 @@
 
 
             function check(arr){
-                var counts = [];
-                for(var i = 0; i <= arr.length; i++) {
-                    if(counts[arr[i]] === undefined || arr[i] == null || arr[i] == "") {
+                let counts = [];
+                for(let i = 0; i <= arr.length; i++) {
+                    if(counts[arr[i]] === undefined || arr[i] == null || arr[i] === "") {
                         counts[arr[i]] = 1;
                     } else {
                         return true;
                     }
                 }
-
                 return false;
             }
         }
