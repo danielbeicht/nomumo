cdcb8fb9a4b4cc68a621fb9b0a75643d2011bc3b
danielbeicht <daniel.beicht@outlook.com>
2018-10-18 18:43:33 +0200

    removed warnings in createdCtrl and unsubscribeCtrl

--- app/components/created/createdCtrl.js
+++ app/components/created/createdCtrl.js
@@ -6,12 +6,11 @@
         .module('nomumo')
         .controller('createdCtrl', createdCtrl);
 
-    createdCtrl.$inject = ['$scope', '$stateParams', '$location', '$translate'];
+    createdCtrl.$inject = ['$scope', '$stateParams', '$location'];
 
-    function createdCtrl($scope, $stateParams, $location, $translate) {
-        var splittedURL=$location.absUrl().split('/');
+    function createdCtrl($scope, $stateParams, $location) {
+        let splittedURL=$location.absUrl().split('/');
         $scope.pollURL=splittedURL[0]+'//'+splittedURL[2]+'/'+'poll'+'/'+splittedURL[4];
         $scope.pollID=$stateParams.pollID;
     }
-
 })();
